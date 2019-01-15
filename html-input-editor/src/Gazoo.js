76c23aa536835495f4361ef806bef15feadfebc8
Dally Wally <dallin.watac.major@gmail.com>
2019-01-14 18:05:27 -0700

    Solved the problem in a lot easier way!

--- html-input-editor/src/Gazoo.js
+++ html-input-editor/src/Gazoo.js
@@ -11,7 +11,13 @@ class Gazoo extends Component {
 
     handleCharater = (char) => {
         console.log(char)
-        if (char === '>' || char === '<') {alert('Sorry but that character is not allowed')};
+        if (char === '>' || char === '<') {
+            alert('Sorry but that character is not allowed')
+        } else {
+            this.setState({
+                inputVal: this.state.inputVal + char
+            })
+        }
 
     }
 
@@ -52,17 +58,30 @@ class Gazoo extends Component {
         }
     }
 
+    handleOnChange = (e) => {
+        console.log(this.state.inputVal.split(''));
+        this.setState({
+            inputVal: e.target.value,
+            htmlVal: '<p>' + e.target.value.replace(/(?:\r\n|\r|\n)/g, '<br/>') + '</p>'
+        });
+    }
+
     handleSubmit = () => {
 
     };
+
+    createMarkUp = () => {
+        return {__html: this.state.htmlVal};
+    }
 
     render() {
         return (
             <div>
                 <div className="form-group">
                     <label for="exampleFormControlTextarea1">Text Area</label>
-                    <textarea className="form-control" onKeyDown={this.HTMLeditorListener} id="exampleFormControlTextarea1" rows="5"></textarea>
+                    <textarea className="form-control" value={this.state.inputVal} onChange={this.handleOnChange} id="exampleFormControlTextarea1" rows="5"></textarea>
                 </div>
+                <div dangerouslySetInnerHTML={this.createMarkUp()}/>
             </div>
         )
     };
