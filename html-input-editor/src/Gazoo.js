c4ea419f33351e4b8cd03b1b588e1663155974b3
Dally Wally <dallin.watac.major@gmail.com>
2019-01-15 11:07:18 -0700

    removed unused methods on Gazoo

--- html-input-editor/src/Gazoo.js
+++ html-input-editor/src/Gazoo.js
@@ -9,55 +9,6 @@ class Gazoo extends Component {
         }
     }
 
-    handleCharater = (char) => {
-        console.log(char)
-        if (char === '>' || char === '<') {
-            alert('Sorry but that character is not allowed')
-        } else {
-            this.setState({
-                inputVal: this.state.inputVal + char
-            })
-        }
-
-    }
-
-    addBreakTag() {
-        this.setState({
-            htmlVal: this.state.htmlVal.splice(this.state.htmlVal.length - 5, 0, '<br/>')
-        });
-    }
-
-    addPtag() {
-        this.setState({
-            htmlVal: this.state.htmlVal + '</p><p>'
-        }) 
-    }
-
-    handleEnter = () => {
-        let endCharater = this.state.htmlVal.charAt(this.state.htmlVal.length - 1);
-        this.state.htmlVal.charAt(endCharater) === '>' ? this.addBreakTag() : this.addPtag();
-    }
-
-    handleBackspace = () => {
-
-    }
-
-    HTMLeditorListener = (event) => {
-        console.log(event);
-        event.preventDefault();
-       
-        switch(event.key) {
-            case 'Enter':
-                this.handleEnter();
-            break;
-            case 'Backspace': 
-                this.handleBackspace();
-            break;
-            default:
-                this.handleCharater(event.key)
-        }
-    }
-
     handleOnChange = (e) => {
         console.log(this.state.inputVal.split(''));
         this.setState({
@@ -65,10 +16,6 @@ class Gazoo extends Component {
             htmlVal: '<p>' + e.target.value.replace(/(?:\r\n|\r|\n)/g, '<br/>') + '</p>'
         });
     }
-
-    handleSubmit = () => {
-
-    };
 
     createMarkUp = () => {
         return {__html: this.state.htmlVal};
