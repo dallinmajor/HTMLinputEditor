d62a3c0c83db48381bf16b2dfda135135d2104a7
Dally Wally <dallin.watac.major@gmail.com>
2019-01-16 20:50:48 -0700

    Updates

---
+++ precache-manifest.014fc4396e193514bd6307140765bfa8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "664e576b2c70e785f872",
+    "url": "/HTMLinputEditor/static/js/runtime~main.664e576b.js"
+  },
+  {
+    "revision": "e064b7cee540ee73b3d1",
+    "url": "/HTMLinputEditor/static/js/main.e064b7ce.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/HTMLinputEditor/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "e064b7cee540ee73b3d1",
+    "url": "/HTMLinputEditor/static/css/main.54d08dc1.chunk.css"
+  },
+  {
+    "revision": "03c681e5f4dc9491a380b7db4dbb3a2c",
+    "url": "/HTMLinputEditor/index.html"
+  }
+];
